024c37964ff328c37df70c06cfe2b80ffc5b22a9
Mariya Iushkova <mariesiushkova@gmail.com>
2025-03-15 14:05:33 +0300

    add Auth page

---
+++ src/app/pages/auth/authtorization/authtorization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authtorization',
+  imports: [],
+  templateUrl: './authtorization.component.html',
+  styleUrl: './authtorization.component.scss',
+})
+export class AuthtorizationComponent { }
