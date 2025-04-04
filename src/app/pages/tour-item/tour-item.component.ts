417495455ea2fbedef5a40d793f426946617f999
sun-funny <mariesiushkova@gmail.com>
2025-04-04 15:33:40 +0300

    directive

--- src/app/pages/tour-item/tour-item.component.ts
+++ src/app/pages/tour-item/tour-item.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ToursService } from '../../services/tours.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule, NgIf } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ITour } from '../../models/tours';
 
 @Component({
   selector: 'app-tour-item',
-  imports: [],
+  imports: [ButtonModule, CommonModule, NgIf, CardModule, RouterLink],
   templateUrl: './tour-item.component.html',
   styleUrl: './tour-item.component.scss'
 })
 export class TourItemComponent implements OnInit {
   tourId: string = null;
+  tour: ITour;
   constructor(private tourService: ToursService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
